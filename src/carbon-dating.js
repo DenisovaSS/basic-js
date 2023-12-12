4ded732170343384af912953b32dc68d40479f4b
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-12-12 16:31:49 +0200

    feat: add second decision

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,36 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  const NumsampleActivity = Number(sampleActivity);
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  let k = Math.LN2 / HALF_LIFE_PERIOD;
+  let age = Math.log(MODERN_ACTIVITY / NumsampleActivity) / k;
+  if (
+    typeof sampleActivity === "string" &&
+    typeof NumsampleActivity === "number" &&
+    age > 0 &&
+    Number.isFinite(age)
+  ) {
+    return Math.ceil(age);
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
