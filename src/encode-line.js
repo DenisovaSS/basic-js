244c26a594c6ce450cb26ce493107b946d35b2c8
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-12-24 13:14:32 +0200

    feat: add encode-line decision

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+let str = "abbcca";
+function encodeLine(str) {
+  let newStr = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        newStr += count + str[i];
+      } else {
+        newStr += str[i];
+      }
 
+      count = 1;
+    }
+  }
+  return newStr;
+  // const unicStr = [...new Set(str)];
+  // let newStr = "";
+  // unicStr.forEach((elem) => {
+  //   let count = str.split("").filter((oneEl) => oneEl === elem).length;
+  //   if (count > 1) {
+  //     newStr = newStr + `${count}${elem}`;
+  //   } else {
+  //     newStr = newStr + elem;
+  //   }
+  // });
+  // return newStr;
+}
+let r = encodeLine(str);
+console.log(r);
 module.exports = {
-  encodeLine
+  encodeLine,
 };
