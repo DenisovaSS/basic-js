565fa460a6752f0f68e92b05eccf7d90db1780e0
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-12-24 13:24:30 +0200

    feat: add get-email decision

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an email address, return it's domain.
@@ -10,11 +10,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const email = "very.unusual.@.unusual.com@usual.com";
+function getEmailDomain(email) {
+  let result = email.split("@");
+  if (result.length > 2) {
+    return result[result.length - 1];
+  } else {
+    return result[1];
+  }
 }
-
+let r = getEmailDomain(email);
+console.log(r);
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
