0a85b96bccbc7e11fbf60273e6c837533ee2cc60
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-12-24 17:01:03 +0200

    feat: add extended-repeater decision

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+const str = true,
+  options = {
+    repeatTimes: 3,
+    separator: "??? ",
+    addition: false,
+    additionRepeatTimes: 2,
+    additionSeparator: "!!!",
+  };
+function repeater(str, options) {
+  str = String(str);
 
+  // console.log(str);
+  let separator = options.separator ? options.separator : "+";
+  let additionSeparator = options.additionSeparator
+    ? options.additionSeparator
+    : "|";
+  let addition = Object.hasOwn(options, "addition")
+    ? String(options.addition)
+    : "";
+  if (addition) {
+    let array = Array(options.additionRepeatTimes).fill(addition);
+    // console.log(array);
+    let fillStr = array.join(additionSeparator);
+    let arrayMain = Array(options.repeatTimes).fill(str + fillStr);
+    let fillStrMain = arrayMain.join(separator);
+    return fillStrMain;
+  } else {
+    let arrayMain = Array(options.repeatTimes).fill(str);
+    let fillStrMain = arrayMain.join(separator);
+    return fillStrMain;
+  }
+}
+const r = repeater(str, options);
+console.log(r);
 module.exports = {
-  repeater
+  repeater,
 };
