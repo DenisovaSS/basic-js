37c67deacae27ba7ad7538be166ee7951ef608eb
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-12-24 15:21:51 +0200

    feat: add sum-digit decision

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const n = 98;
+function getSumOfDigits(n) {
+  let summ = 0;
+  while (n) {
+    summ += n % 10;
+    n = Math.floor(n / 10);
+  }
+  if (summ.toString().length > 1) {
+    return getSumOfDigits(summ);
+  } else {
+    return summ;
+  }
+  // console.log((n - 1) % 9);
+  // return ((n - 1) % 9) + 1;
 }
-
+let r = getSumOfDigits(n);
+console.log(r);
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
